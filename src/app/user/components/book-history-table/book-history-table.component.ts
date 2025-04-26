98b0f3cc0f7eeaa2244726c150aa1845c836d207
sarathsuryas <sarathsuryasss48@gmail.com>
2025-04-26 20:45:22 +0530

    completed

--- src/app/user/components/book-history-table/book-history-table.component.ts
+++ src/app/user/components/book-history-table/book-history-table.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -10,36 +9,58 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-book-history-table',
   standalone: true,
-  imports: [CommonModule,RouterModule, MatTableModule, MatPaginatorModule,MatButtonModule],
+  imports: [CommonModule, RouterModule, MatTableModule, MatPaginatorModule, MatButtonModule],
   templateUrl: './book-history-table.component.html',
-  styleUrl: './book-history-table.component.css'
+  styleUrls: ['./book-history-table.component.css'], // Fixed "styleUrls" property
 })
 export class BookHistoryTableComponent {
   displayedColumns: string[] = ['bookNo', 'title', 'librarian', 'action'];
   dataSource = new MatTableDataSource<any>();
+  isReturning = false; // Track the return button state
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
   constructor(private _bookService: BookService) {
     this.loadBookHistory();
   }
 
   ngAfterViewInit() {
+    this.paginator.pageSize = 10; // Set default page size
     this.dataSource.paginator = this.paginator;
   }
 
+  // Load book history data
   loadBookHistory() {
-    this._bookService.bookHistory().subscribe((data) => {
-      this.dataSource.data = data; 
+    this._bookService.bookHistory().subscribe({
+      next: (data) => {
+        this.dataSource.data = data;
+      },
+      error: (err) => {
+        console.error('Error fetching book history:', err);
+      },
     });
   }
 
   // Return a book
-  return(element: any) {
-    this._bookService.return(element.bookData._id).subscribe(() => {
-      this.loadBookHistory(); 
+  return(element: BookData) {
+    this.isReturning = true;
+    this._bookService.return(element._id).subscribe({
+      next: () => {
+        this.isReturning = false;
+        this.loadBookHistory();
+      },
+      error: (err) => {
+        this.isReturning = false;
+        console.error('Error returning book:', err);
+      },
     });
   }
 }
 
-
+// Define the interface for book data
+export interface BookData {
+  _id: string;
+  bookNo: string;
+  title: string;
+  librarian: string;
+}
